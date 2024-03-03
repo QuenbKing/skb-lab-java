22ad8f7d57b76af0cf23dec6d4e56af6b1e73b4f
QuenbKing <Nikita.Karskanov@urfu.me>
2024-03-03 13:44:44 +0500

    Rename Package

--- src/main/java/ru/nKarskanov/skb/SkbApplication.java
+++ src/main/java/ru/nKarskanov/skb/SkbApplication.java
@@ -1,4 +1,4 @@
-package org.example.skb;
+package ru.nKarskanov.skb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ru/nKarskanov/skb/SkbApplicationTests.java
+++ src/test/java/ru/nKarskanov/skb/SkbApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.skb;
+package ru.nKarskanov.skb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
